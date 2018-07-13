6416f6d387055b6b6b3ae1badfa3f0ef5041806c
saklynour <40861836+saklynour@users.noreply.github.com>
2018-07-13 16:39:41 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,99 @@
+
+$(document).ready(function(){
+    $("#villa2").click(function(){
+      
+        $("#p1").css("visibility","visible");
+        $("#p1").toggle(300);
+    });
+})
+$(document).ready(function(){
+    $("#villa1").click(function(){
+      
+        $("#p2").css("visibility","visible");
+        $("#p2").toggle(300);
+    });
+})
+$(document).ready(function(){
+    $("#villa").click(function(){
+      
+        $("#p3").css("visibility","visible");
+        $("#p3").toggle(300);
+    });
+})
+$(document).ready(function(){
+    $("#lol").hover(function(){
+      
+        $("#box2").css({"transform": "translate(-550px,0)", "opacity": "1"});
+       
+    });
+})
+
+    $("#mdr").hover(function(){
+      
+        $("#box1").css({"transform": "translate(550px,0)", "opacity": "1"});
+      
+    });
+
+
+    $("#xd").hover(function(){
+      
+        $("#box").css({"transform": "translate(-550px,0)", "opacity": "1"})
+    });
+    $("#nintendo").click(function(){
+$(".legend").css("display", "none")
+$(".pics1").css("display","none")
+$("#nintendo").css("display", "none")
+    })
+
+
+
+
+
+$(".pics").click(function(){
+    if(
+   
+        $("#pic").click(function(){
+            $(".pics1").css("display","none")
+    $("#pic7").css("display","block")
+    $(".legend").css("display", "block")
+    $("#nintendo").css("display", "block")
+    }));
+    if(
+        $("#pic1").click(function(){
+            $(".pics1").css("display","none")
+         $("#pic6").css("display","block")
+         $(".legend").css("display", "block")
+         $("#nintendo").css("display", "block")
+         }));
+         if(
+            $("#pic2").click(function(){
+                $(".pics1").css("display","none")
+             $("#pic8").css("display","block")
+             $(".legend").css("display", "block")
+             $("#nintendo").css("display", "block")
+             }));
+             if(
+                $("#pic3").click(function(){
+                    $(".pics1").css("display","none")
+                 $("#pic9").css("display","block")
+                 $(".legend").css("display", "block")
+                 $("#nintendo").css("display", "block")
+                 }));
+                 if(
+                    $("#pic4").click(function(){
+                        $(".pics1").css("display","none")
+                     $("#pic7").css("display","block")
+                     $(".legend").css("display", "block")
+                     $("#nintendo").css("display", "block")
+                     }));
+                     if(
+                        $("#pic5").click(function(){
+                            $(".pics1").css("display","none")
+                         $("#pic6").css("display","block")
+                         $(".legend").css("display", "block")
+                         $("#nintendo").css("display", "block")
+                         }));
+   });
+
+
+    
